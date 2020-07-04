08c4d8d85a67c919bf2dc467f2e9773ce5ecbb27
syu-h81 <58079996+syu-h81@users.noreply.github.com>
2020-07-05 01:31:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ $(function() {
   $(window).scroll(function(){
     var motion = $(this).scrollTop();
     $('#bg').css('background-position', '0 ' + parseInt( -motion / 5 ) + 'px');
+    $('#safety-bg').css('background-position', '0 ' + parseInt( -motion / 7 ) + 'px');
   });
 });
